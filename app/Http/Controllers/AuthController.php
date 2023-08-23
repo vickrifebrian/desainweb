f1ef9e09b5fba7bdc84ddb2bbb3582ca38d47694
vickrifebrian <vickrifebrian7@gmail.com>
2023-08-23 14:06:09 +0700

    first commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    function BISMILLAH()
+    {
+      return view('test.test');
+    }
+}
